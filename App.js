03f24acf2b6dc601bfa89e5931879c7390053109
prathyushanasina <nasinaprathyusha00@gmail.com>
2024-02-26 17:24:57 +0530

    lesson-1

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = [React.createElement('div', {id:"root",className:"new"},[
+    React.createElement('h1',{id:'new'},'I am heding 1'),
+    React.createElement('h2',{id:'new'},'I am heding 2')
+]),React.createElement('div', {id:"root",className:"new"},[
+    React.createElement('h1',{id:'new'},'I am heding 1'),
+    React.createElement('h2',{id:'new'},'I am heding 2')
+])];
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
